95cab08a7290f8193828056e6dba356faca302db
Niamh <niamhimurphy606@gmail.com>
2026-08-10 15:56:17 +0100

    figuring out how spacing works on the webpage

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-pink font-mono dark:bg-pink">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-pink dark:bg-pink sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            This is the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-pink/[.08]">
-              About Page
-            </code>{" "}
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            penis penis balls
-          </p>
+    <main>
+      <div className="grid grid-flow-col grid-rows-4 gap-4">
+        <div className="flex items-center gap-4 rounded-lg bg-white p-6 shadow-md outline outline-black/5 dark:bg-gray-800">
+          <span className="inline-flex shrink-0 rounded-full border border-pink-300 bg-pink-100 p-2 dark:border-pink-300/10 dark:bg-pink-400/10">
+            <svg className="size-6 stroke-pink-700 dark:stroke-pink-500"></svg>
+          </span>
+          <div>
+            <p className="text-gray-700 dark:text-gray-400">
+              <span className="font-medium text-gray-950 dark:text-white">Tom Watson</span> mentioned you in
+              <span className="font-medium text-gray-950 dark:text-white">Logo redesign</span>
+            </p>
+          </div>
         </div>
-      </main>
-    </div>
+        <div className="flex items-center gap-4 rounded-lg bg-white p-6 shadow-md outline outline-black/5 dark:bg-gray-800">
+          <span className="inline-flex shrink-0 rounded-full border border-pink-300 bg-pink-100 p-2 dark:border-pink-300/10 dark:bg-pink-400/10">
+            <svg className="size-6 stroke-pink-700 dark:stroke-pink-500"></svg>
+          </span>
+          <div>
+            <p className="text-gray-700 dark:text-gray-400">
+              <span className="font-medium text-gray-950 dark:text-white">Tom Watson</span> mentioned you in
+              <span className="font-medium text-gray-950 dark:text-white">Logo redesign</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
